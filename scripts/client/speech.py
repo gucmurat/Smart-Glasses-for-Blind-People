6d136882496ec849a11ffc3d416826ffe2098cf7
gucmurat <mguc18@ku.edu.tr>
2023-11-26 01:11:47 +0300

    Initial commit.

---
+++ scripts/client/speech.py
@@ -0,0 +1,33 @@
+import os
+import platform
+from fastapi import FastAPI, HTTPException, Request
+
+def text_to_speech(tx):
+    tx = repr(tx)
+    syst = platform.system()
+    if syst == 'Linux' and platform.linux_distribution()[0] == "Ubuntu":
+        os.system('spd-say %s' % tx)
+    elif syst == 'Windows':
+        os.system('PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(%s);"' % tx)
+    elif syst == 'Darwin':
+        os.system('say %s' % tx)
+    else:
+        raise RuntimeError("Operating System '%s' is not supported" % syst)
+
+app = FastAPI()
+
+@app.post("/send_text")
+async def send_text_to_client(info:Request):
+    info_json = await info.json()
+    text = info_json['text']
+    if not text:
+        raise HTTPException(status_code=400, detail="Text cannot be empty")
+    
+    return text_to_speech(text)
+
+"""
+import requests
+url = "http://127.0.0.1:8000/send_text"
+data = {"text": "HelloFromClient"}
+response = requests.post(url, json=data)
+"""
